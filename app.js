1ff1a16f5d241849ac826f9be3e44d74c5e7d4af
jcassola <jcassola96@gmail.com>
2023-04-02 16:43:16 -0400

    deploy react app

--- app.js
+++ app.js
@@ -13,28 +13,28 @@ hbs.registerPartials(__dirname + '/views/partials');
 // Servir contenido estatico
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render('home', {
-    nombre: 'Obi-wan',
-    titulo: 'Hello there'
-  });
-});
-app.get("/generic", (req, res) => {
-  res.render('generic', {
-    nombre: 'Obi-wan',
-    titulo: 'Hello there'
-  });
-});
-app.get("/elements", (req, res) => {
-  res.render('elements', {
-    nombre: 'Obi-wan',
-    titulo: 'Hello there'
-  });
-});
-
-// app.get("*", (req, res) => {
-//   res.sendFile(__dirname + "/public/404.html");
+// app.get("/", (req, res) => {
+//   res.render('home', {
+//     nombre: 'Obi-wan',
+//     titulo: 'Hello there'
+//   });
 // });
+// app.get("/generic", (req, res) => {
+//   res.render('generic', {
+//     nombre: 'Obi-wan',
+//     titulo: 'Hello there'
+//   });
+// });
+// app.get("/elements", (req, res) => {
+//   res.render('elements', {
+//     nombre: 'Obi-wan',
+//     titulo: 'Hello there'
+//   });
+// });
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
